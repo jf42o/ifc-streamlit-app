0aead77ab9d47b1ba97a5a8d920edd6ef9997c71
Jannik Fath <jafath@web.de>
2022-12-26 17:01:08 +0100

    fix excel download button

--- pages/quantities.py
+++ pages/quantities.py
@@ -2,9 +2,23 @@ import streamlit as st
 from tools import ifchelper
 from tools import pandashelper
 from tools import graph_maker
+from io import BytesIO
+from pyxlsb import open_workbook as open_xlsb
+import pandas as pd
 
 session = st.session_state
 
+def to_excel(df):
+    output = BytesIO()
+    writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    df.to_excel(writer, index=False, sheet_name='Sheet1')
+    workbook = writer.book
+    worksheet = writer.sheets['Sheet1']
+    format1 = workbook.add_format({'num_format': '0.00'}) 
+    worksheet.set_column('A:A', None, format1)  
+    writer.save()
+    processed_data = output.getvalue()
+    return processed_data
 
 def initialize_session_state():
     session["DataFrame"] = None
@@ -71,10 +85,8 @@ def execute():
             class_selector_dataframe = st.multiselect("select all classes", session.Classes, default = session.Classes, key="class_selector_dataframe")
             session["filtered_frame_df"] = pandashelper.filter_dataframe_per_class_df(session.DataFrame, class_selector_dataframe)
             st.write(session["filtered_frame_df"])
-            st.button("📄 download csv", key="download_csv_dataframe", on_click=download_csv_df)
-            st.button("📗 download excel", key="download_excel_dataframe", on_click=download_excel_df)
-            #CHANGED
-            st.download_button("download", session["filtered_frame_df"].to_csv(), session.file_name.replace(".ifc", ".csv"))
+            st.download_button("📗 download excel", to_excel(session["filtered_frame_df"]), session.file_name.replace(".ifc", ".xlsx"))
+            st.download_button("📄 download csv", session["filtered_frame_df"].to_csv(), session.file_name.replace(".ifc", ".csv"))
         with tab2:
             row2col1, row2col2 = st.columns(2)
             with row2col1:
@@ -83,29 +95,30 @@ def execute():
                     session["filtered_frame_qto"] = pandashelper.filter_dataframe_per_class_qto(session.DataFrame, class_selector_qto)
                     session["qtos"] = pandashelper.get_qsets_columns(session["filtered_frame_qto"])
                     if session["qtos"] is not None:
-                        qto_selector = st.selectbox("select Quantity Set", session.qtos, key='qto_selector')
-                        quantities = pandashelper.get_quantities(session.filtered_frame_qto, qto_selector)
+                        qto_selector1 = st.selectbox("select Quantity Set", session.qtos, key='qto_selector')
+                        quantities = pandashelper.get_quantities(session.filtered_frame_qto, qto_selector1)
                         st.selectbox("select quantity", quantities, key="quantity_selector_qto")
                         st.radio('split per', ['Level', 'Type'], key="split_options")
                     else:
                         st.warning("no quantities to look at !")
             ## DRAW FRAME
             with row2col2: 
-                if "quantity_selector" in session and session.quantity_selector == "Count":
+                if "quantity_selector_qto" in session and session.quantity_selector_qto == "Count":
                     total = pandashelper.get_total(session.filtered_frame_qto)
                     st.write(f"The total number of {session.class_selector_qto} is {total}")
                 else:
                     if session.qtos is not None:
-                        st.subheader(f"{session.class_selector_qto} {session.quantity_selector}")
+                        st.subheader(f"{session.class_selector_qto} {session.quantity_selector_qto}")
                         graph = graph_maker.load_graph(
                             session.filtered_frame_qto,
                             session.qto_selector,
-                            session.quantity_selector,
+                            session.quantity_selector_qto,
                             session.split_options                            
                         )
                         st.plotly_chart(graph)
-            st.button("📄 download csv", key="download_csv_qto", on_click=download_csv_qto)
-            st.button("📗 download excel", key="download_excel_qto", on_click=download_excel_qto)
+            st.download_button("📄 download csv", session["filtered_frame_qto"].to_csv(), session.file_name.replace(".ifc", ".csv"))
+            st.download_button("📗 download excel", to_excel(session["filtered_frame_qto"]), session.file_name.replace(".ifc", ".xlsx"))
+
             #st.download_button("download", session["filtered_frame_df"].to_csv())
     else: 
         st.header("please load a file from the home menu")
